148fe867bf29f898e42e85b138c5e0a78c7b4489
Tushr singh <singhtusharji522@gmail.com>
2026-07-31 00:23:55 +0530

    Add files via upload

---
+++ Untitled13.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from contact import Contact
+
+contact_list = []
+
+while True:
+    print("1. Add Contact")
+    print("2. Show All Contacts")
+    print("3. Delete Contact")
+    print("4. Search Contact")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
