fc998675ea8e0a48ab1ec9d51374164705cbe206
Tushr singh <singhtusharji522@gmail.com>
2026-07-31 00:17:02 +0530

    Add files via upload

---
+++ Show All Contact.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Show All contact --------------------------------------------------------------------------------------------------------------------
+
+   elif choice == '2':
+
+           if len(contact_list) == 0:
+               print("no contact found")
+           else:
+               for c in contact_list:
+                   c.display()
+
